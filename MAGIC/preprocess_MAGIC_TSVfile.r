96f2a5fbd51c12f12f63106beca8ff82a397e401
Mogens <30597713+mognHLU@users.noreply.github.com>
2017-08-18 09:03:34 +0200

    Update preprocess_MAGIC_TSVfile.r

--- MAGIC/preprocess_MAGIC_TSVfile.r
+++ MAGIC/preprocess_MAGIC_TSVfile.r
@@ -1,4 +1,4 @@
-##########################################################
+##################################################################
 # 
 # FILE          : preprocess_MAGIC_TSVfile.r
 #
@@ -9,13 +9,13 @@
 # DESCRIPTION   : Loads the MAGIC .tsv file and rearranges the 
 #                 column order, and writes the resulting
 #                 table as a .tsv file 
-#########################################################
+##################################################################
 
 # Input file path 
 infp <- "M:/Bioinformatics/TranSMART/GWAS/mod_MAGIC_2hrGlucose_AdjustedForBMI.tsv"
 
 # Output file path 
-outfp <- "M:/Bioinformatics/TranSMART/GWAS/MAGIC_mod/mod_MAGIC_2hrGlucose_AdjustedForBMI.tsv"
+outfp <- "A:/Public Databases/MAGIC/MAGIC_2hrGlucose_AdjustedForBMI.tsv"
 
 # Importing a .tsv file as a data frame
 # Drop last empty column 
@@ -24,10 +24,11 @@ tsv_df <- read.table(file = infp
 
 # Reordering the data set by column name 
 # The order of the column names is tranSMART specified 
-tsv_df <- tsv_df[c("P_VALUE", "RS_ID", "ODDS_RATIO", "BETA", "DIRECTION_OF_EFFECT", "HWE_CONTROLS", "HWE_CASES",
-                   "HWE_TOTAL_STUDIES", "MAF", "GENOTYPE_COUNTS_CASES", "GENOTYPE_COUNTS_CONTROLS", 
-                   "TOTAL_NUMBER_GENOTYPED", "ALLELE1", "ALLELE2", "GOOD_CLUSTERING", "EFFECT", 
-                   "STANDARD_ERROR", "HETISQ", "HETPVAL", "GENOTYPE_IMPUTED", "RSQ", "DOSE.B.0", "DOSE.B.1")]
+tsv_df <- tsv_df[c("P_VALUE", "RS_ID", "MAF", "STANDARD_ERROR", "HWE_CASES", "GENOTYPE_IMPUTED",
+                   "EFFECT", "DOSE.B.0", "DOSE.B.1", "HWE_TOTAL_STUDIES", "TOTAL_NUMBER_GENOTYPED",
+                   "GOOD_CLUSTERING", "GENOTYPE_COUNTS_CASES", "ODDS_RATIO", "GENOTYPE_COUNTS_CONTROLS",
+                   "HETISQ", "HWE_CONTROLS", "ALLELE2", "BETA", "ALLELE1", "DIRECTION_OF_EFFECT",  
+                   "RSQ", "HETPVAL")]
 
 # Writing the modified input as .tsv file 
 write.table(tsv_df, file = outfp, 
